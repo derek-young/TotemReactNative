fd75ac60c22bc8b60c4428074221b4bb53f8840c
Derek Young <dthomasy@gmail.com>
2017-02-16 19:28:13 -0800

    Test views

---
+++ app/components/CreateGroup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CreateGroup = () => (
+  <View>
+    <Text>Test CreateGroup View</Text>
+  </View>
+);
+
+export defualt CreateGroup;
---
+++ app/components/InviteFriends.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const InviteFriends = () => (
+  <View>
+    <Text>Test InviteFriends View</Text>
+  </View>
+);
+
+export defualt InviteFriends;
