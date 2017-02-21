96d40abf5e344b03f464c9f676271cd89ec1a413
John Currie <johntcurrie@gmail.com>
2017-02-21 09:27:53 -0800

    (feat/login) Added states to login

--- index.ios.js
+++ index.ios.js
@@ -6,9 +6,18 @@ import store from './app/store.js';
 import FBSDK, { LoginManager } from 'react-native-fbsdk';
 
 export default class Rabble extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    }
+  }
   _fbAuth(){
+    var context = this;
     LoginManager.logInWithReadPermissions(['public_profile']).then(function(results){
-      if(isCancelled) {
+        context.setState({isLoggedIn: true})
+      if(result.isCancelled) {
         console.log('Log in cancelled')
       } else {
         console.log('Login Successful')
@@ -18,6 +27,7 @@ export default class Rabble extends React.Component {
     })
   }
   render() {
+    if(this.state.isLoggedIn === false) {
     return (
       <View style={{ flex: 1 }}>
       <Text></Text>
@@ -26,15 +36,23 @@ export default class Rabble extends React.Component {
       <Text></Text>
       <Text></Text>
       <Text></Text>
-      <TouchableOpacity onPress={this._fbAuth}>
+      <TouchableOpacity onPress={this._fbAuth.bind(this)}>
      <Text>Login with Facebook</Text>
      </TouchableOpacity>
       </View>
     );
   }
+  else {
+    return(
+    <View style={{ flex: 1 }}>
+      <Provider store={store}>
+      <App />
+      </Provider>
+    </View>
+    )
+  }
+  }
 }
 
 AppRegistry.registerComponent('Rabble', () => Rabble);
-   // <Provider store={store}>
-   //        <App />
-   //      </Provider>
+
