a5ac633f83f8c7cb236f2a5f5b668086bec98765
John Currie <johntcurrie@gmail.com>
2017-02-21 20:23:43 -0800

    Git merge

--- index.ios.js
+++ index.ios.js
@@ -6,67 +6,44 @@ import store from './app/store.js';
 import FBSDK, { LoginManager, AccessToken } from 'react-native-fbsdk';
 
 export default class Rabble extends React.Component {
-
-
-<<<<<<< HEAD
   constructor(props){
     super(props);
     this.state = {
-      isLoggedIn: true
+      isLoggedIn: false
     }
   }
   _fbAuth(){
     var context = this;
-    LoginManager.logInWithReadPermissions(['public_profile', 'email']).then(function(result){
+    LoginManager.logInWithReadPermissions(['public_profile']).then(function(result){
         context.setState({isLoggedIn: true})
       if(result.isCancelled) {
         console.log('Log in cancelled')
       } else {
         console.log('Login Successful')
+        //AccessToken.getCurrentAccessToken().then
       }
     }, function(error){
       console.log('Error signing in', error);
     })
   }
-=======
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     isLoggedIn: false
-  //   }
-  // }
-  // _fbAuth(){
-  //   var context = this;
-  //   LoginManager.logInWithReadPermissions(['public_profile']).then(function(result){
-  //       context.setState({isLoggedIn: true})
-  //     if(result.isCancelled) {
-  //       console.log('Log in cancelled')
-  //     } else {
-  //       console.log('Login Successful')
-  //     }
-  //   }, function(error){
-  //     console.log('Error signing in', error);
-  //   })
-  // }
->>>>>>> 2b8340d710314da2b4d308f597661e9660b9eb61
 
   render() {
-  //   if(this.state.isLoggedIn === false) {
-  //   return (
-  //     <View style={{ flex: 1 }}>
-  //     <Text></Text>
-  //     <Text></Text>
-  //     <Text></Text>
-  //     <Text></Text>
-  //     <Text></Text>
-  //     <Text></Text>
-  //     <TouchableOpacity onPress={this._fbAuth.bind(this)}>
-  //    <Text>Login with Facebook</Text>
-  //    </TouchableOpacity>
-  //     </View>
-  //   );
-  // }
-  // else {
+    if(this.state.isLoggedIn === false) {
+    return (
+      <View style={{ flex: 1 }}>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <TouchableOpacity onPress={this._fbAuth.bind(this)}>
+     <Text>Login with Facebook</Text>
+     </TouchableOpacity>
+      </View>
+    );
+  }
+  else {
     return(
     <View style={{ flex: 1 }}>
       <Provider store={store}>
@@ -74,7 +51,7 @@ export default class Rabble extends React.Component {
       </Provider>
     </View>
     )
-  // }
+  }
   }
 }
 
